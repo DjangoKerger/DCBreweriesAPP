ac700b2205731a8752c2245377ed19dca7bb5d7e
“First <Kerger06@aol.com>
2020-05-04 11:36:22 -0500

    added second page, removed description

--- js/constructor.js
+++ js/constructor.js
@@ -17,9 +17,10 @@ const listContainer = document.getElementById("listContainer")
 
 const brewery = sampleDataArray.map(function(brew){
     return `<h1>${brew.brewery.name}</h1>
-            <p> ${brew.brewery.description}</p>
+            <a href="2ndpage.html"><img src="${brew.brewery.images.large}" alt=${brew.brewery.id}></a>
+            <p> ${brew.brewery.}
             <a href ="${brew.brewery.website}">Visit the website</a>`
-
+            
 })
 
 listContainer.innerHTML = brewery.join(" ")
