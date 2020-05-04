377255150eafd86a87e2f47684f9e913941db6ad
Steve Medrano <60121429+darknight101-pixel@users.noreply.github.com>
2020-05-04 00:05:37 -0500

    google map appear in screen

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,1 +1,35 @@
 //console.log('Will we be able to pull this one off?')
+
+//let mapContainer = document.getElementById("mapContainer")
+
+
+
+
+document.body.innerHTML='<div id="map-canvas" style="width:1000px; height:500px;"></div>'
+
+// the option in the map
+    function initMap() {
+        var mapOptions = {
+            zoom: 8,
+            center: new google.maps.LatLng(-34.397, 150.644)
+    };
+
+//new map
+    var map = new google.maps.Map(document.getElementById("map-canvas"), 
+    mapOptions);
+    }
+   /* // marker
+    var marker = new google.maps.Marker({
+      position:{lat:29.7604,lng:95.3698},
+      map:map,
+      icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+    }); */ 
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAlkt5RglHfe4Dt_NcVr-3S6HJ8Yo47WIE&callback=initMap';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
