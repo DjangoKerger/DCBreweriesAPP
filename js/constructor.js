ef54777d02cc29e05b779f6a6900712fed2aea8d
“First <Kerger06@aol.com>
2020-05-04 10:03:51 -0500

    added for loop and map to give us a choice, displaying information with template literals

--- js/constructor.js
+++ js/constructor.js
@@ -1,1 +1,25 @@
-//console.log('Costruction is Important!')
+const listContainer = document.getElementById("listContainer")
+
+
+// for(let index = 0; index < sampleDataArray.length; index++){
+
+//     let brew = sampleDataArray[index]
+
+//     let brewerie = `<li>
+//                 <img src="${brew.imageURL}" />
+//                 <b>${brew.brewery.name}</b>
+//                 <p>${brew.brewery.description}</p>
+//                 <b>${brew.streetAddress}</b>
+
+//                 </li>`
+//     listContainer.insertAdjacentHTML('beforeend',brewerie)
+// }
+
+const brewery = sampleDataArray.map(function(brew){
+    return `<h1>${brew.brewery.name}</h1>
+            <p> ${brew.brewery.description}</p>
+            <a href ="${brew.brewery.website}">Visit the website</a>`
+
+})
+
+listContainer.innerHTML = brewery.join(" ")
