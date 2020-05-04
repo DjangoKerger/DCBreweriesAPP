711d685663b2de4f810294d149c46504037ccf38
Hannah Lima <nanaa_lima@hotmail.com>
2020-05-03 20:47:25 -0500

    This was changed to aviod merge conflict with you guys code

--- js/googlemaps.js
+++
@@ -1,5 +0,0 @@
-// How to use the searchByLocation function
-// searchByLocation().then(data => {
-//   -------------The rest of your code-----------------
-//   would start with console.log(data)
-// })
