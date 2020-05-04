082ea30d156cd75375528bb3f6a316613b2f2d8a
Steve Medrano <60121429+darknight101-pixel@users.noreply.github.com>
2020-05-04 13:23:47 -0500

    seperate js script from html

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -2,36 +2,47 @@
 
 let mapContainer = document.getElementById("mapContainer")
 
-
+/*
 document.body.innerHTML='<div id="map-canvas" style="width:1000px; height:500px;"></div>'
+
 
 // the option in the map
     function initMap() {
         var mapOptions = {
             zoom: 8,
-            center: new google.maps.LatLng(29.7604, 95.3698)
+            center: new google.maps.LatLng(-34.397, 150.644)
     };
 
 //new map
     var map = new google.maps.Map(document.getElementById("map-canvas"), 
     mapOptions);
-    }
-
-   /* // marker
-    var marker = new google.maps.Marker({
-      position:{lat:29.7604,lng:95.3698},
-      map:map,
-      icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-    }); */ 
-
+    
+    let marker = new google.maps.Marker({
+        position: mylatlng,
+        map: map,
+    });
+}
+///////////////////////////////////////////
+*/
 function loadScript() {
   var script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAlkt5RglHfe4Dt_NcVr-3S6HJ8Yo47WIE&callback=initMap';
   document.body.appendChild(script);
 }
+//////////////////////////////////////////////
+window.onload = loadScript; 
 
-window.onload = loadScript;
+(function(window, google) {
 
+    // map option
+    var options = {
+        center: {
+            lat: 37.79
+        }
+    }
+    //
+
+}(window,google));
 
 
