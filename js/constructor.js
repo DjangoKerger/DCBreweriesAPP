99e5338e1b3a8baef8b39bfbb414ba76dbbbc377
“First <Kerger06@aol.com>
2020-05-06 09:49:13 -0500

    added margin to the listcontainer div, and padding to cards

--- js/constructor.js
+++ js/constructor.js
@@ -1,5 +1,7 @@
 listContainer = document.getElementById("listContainer")
 
+
+
 let brewery = sampleDataArray.map(function (brew) {
   return `
   <div class="col-lg-3 col-md-6">
@@ -10,8 +12,8 @@ let brewery = sampleDataArray.map(function (brew) {
         <h5>${brew.locationTypeDisplay}</h5>
         <p class="card-text">${brew.streetAddress}, ${brew.locality} ${brew.postalCode}</p>
         <button type="button" class="btn btn-outline-#C47335"><i class="fab fa-facebook-square fa-2x" style="color:#C47335"></i></button><button type="button" class="btn btn-md btn-fb"><i class="fab fa-instagram fa-2x"style="color:#C47335"></i></button><button type="button" class="btn btn-md btn-fb"><i class="fab fa-twitter-square fa-2x"style="color:#C47335"></i></button>
-        
-      
+  </div>      
+  </div>    
   </div>
     
   </div>
@@ -19,6 +21,26 @@ let brewery = sampleDataArray.map(function (brew) {
 
 
 })
+// listContainer.innerHTML = <nav class="navbar navbar-expand-lg navbar-light">
+// <a class="navbar-brand" href="#">BreweryAPP</a>
+// <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+//   <span class="navbar-toggler-icon"></span>
+// </button>
+// <div class="collapse navbar-collapse" id="navbarNavDropdown">
+//   <ul class="navbar-nav">
+//     <li class="nav-item active">
+//       <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+//     </li>
+//     <li class="nav-item">
+//       <a class="nav-link" href="#">Highest rated</a>
+//     </li>
+//     <li class="nav-item">
+//       <a class="nav-link" href="#">closest</a>
+//     </li>
+    
+//   </ul>
+// </div>
+// </nav>
 listContainer.innerHTML = brewery.join(" ")
 
 // ${brew.brewery.name}</h3>
